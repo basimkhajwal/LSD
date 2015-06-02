49ba60f2499405c9bf5717d6a89c79e1663d388d
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-02 21:29:26 +0100

    Added the libGDX project

---
+++ android/src/net/net63/codearcade/LSD/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.net63.codearcade.LSD.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.net63.codearcade.LSD.LSD;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LSD(), config);
+	}
+}
---
+++ desktop/src/net/net63/codearcade/LSD/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.net63.codearcade.LSD.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.net63.codearcade.LSD.LSD;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LSD(), config);
+	}
+}
