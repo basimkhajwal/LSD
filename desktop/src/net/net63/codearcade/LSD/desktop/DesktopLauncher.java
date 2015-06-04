5c8c94a6210f873a516f003ac623b7f1ea1a0c99
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-04 23:04:22 +0100

    Added comments and title

--- desktop/src/net/net63/codearcade/LSD/desktop/DesktopLauncher.java
+++ desktop/src/net/net63/codearcade/LSD/desktop/DesktopLauncher.java
@@ -2,11 +2,23 @@ package net.net63.codearcade.LSD.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import net.net63.codearcade.LSD.LSD;
 
+import net.net63.codearcade.LSD.LSD;
+import net.net63.codearcade.LSD.utils.Constants;
+
+/**
+ * Desktop launcher for LSD
+ * 
+ * @author Basim
+ *
+ */
 public class DesktopLauncher {
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = Constants.TITLE;
+		
 		new LwjglApplication(new LSD(), config);
 	}
 }
