2f708e318982bc7ea4e22c072cbff24b74c9666e
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-06 20:53:39 +0100

    Created new assets file

---
+++ core/src/net/net63/codearcade/LSD/utils/Assets.java
@@ -0,0 +1,29 @@
+package net.net63.codearcade.LSD.utils;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+/**
+ * Class to load and handle all the assets 
+ * 
+ * @author Basim
+ *
+ */
+public class Assets {
+	
+	public static final String BACKGROUND_IMAGE = "img/bg.jpg";
+	
+	private static AssetManager assetManager = new AssetManager();
+	
+	//Private constructor to prevent instantiation
+	private Assets () { }
+	
+	/**
+	 *  Static method that loads all the assets in 
+	 */
+	public static void loadAll() {
+		
+		
+		
+	}
+	
+}
