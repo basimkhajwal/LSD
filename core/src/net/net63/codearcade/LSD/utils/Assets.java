41ba815233a99c2784f0b88fb52dfbe7dc21386e
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-06 21:14:00 +0100

    Setup loading stuff

--- core/src/net/net63/codearcade/LSD/utils/Assets.java
+++ core/src/net/net63/codearcade/LSD/utils/Assets.java
@@ -1,6 +1,9 @@
 package net.net63.codearcade.LSD.utils;
 
+import java.lang.reflect.Field;
+
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
 
 /**
  * Class to load and handle all the assets 
@@ -10,7 +13,9 @@ import com.badlogic.gdx.assets.AssetManager;
  */
 public class Assets {
 	
-	public static final String BACKGROUND_IMAGE = "img/bg.jpg";
+	public static class Images {
+		public static final String BACKGROUND = "images/bg.jpg";
+	}
 	
 	private static AssetManager assetManager = new AssetManager();
 	
@@ -18,12 +23,45 @@ public class Assets {
 	private Assets () { }
 	
 	/**
-	 *  Static method that loads all the assets in 
+	 *  Static method that loads all the assets in the asset classes
 	 */
 	public static void loadAll() {
 		
+		for (Field image: Images.class.getDeclaredFields()) {
+			try {
+				assetManager.load((String) image.get(null), Texture.class);
+			} catch (Exception e) {
+				System.out.println("Invalid file name!!");
+				e.printStackTrace();
+			}
+		}
 		
-		
+		assetManager.finishLoading();
+	}
+	
+	/**
+	 * Get a loaded asset of type T from the loaded cache
+	 * 
+	 * @param fileName The identifier for the asset 
+	 * @param type The type to return
+	 * @return	The asset with the type as specified
+	 */
+	public static <T> T getAsset(String fileName, Class<T> type) {
+		return assetManager.get(fileName, type);
+	}
+	
+	/**
+	 * Dispose all the assets but the Asset Manager still remains (re-usable)
+	 */
+	public static void clear() {
+		assetManager.clear();
+	}
+	
+	/**
+	 * Dispose all the assets as well as the Asset Manager (not resettable)
+	 */
+	public static void dispose() {
+		assetManager.dispose();
 	}
 	
 }
