49100694c5d9c2a496a4d100172655e8f5d76c58
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-23 21:12:26 +0100

    Added player component

--- core/src/net/net63/codearcade/LSD/components/PlayerComponent.java
+++ core/src/net/net63/codearcade/LSD/components/PlayerComponent.java
@@ -1,7 +1,12 @@
 package net.net63.codearcade.LSD.components;
 
+import com.badlogic.ashley.core.Component;
+
 /**
  * Created by Basim on 23/06/15.
  */
-public class PlayerComponent {
+public class PlayerComponent extends Component{
+
+    
+
 }
