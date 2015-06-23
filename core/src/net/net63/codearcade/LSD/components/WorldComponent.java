43363349f2ab7c0e3512d418afc2cb888f20460c
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-23 21:19:08 +0100

    Added new components

--- core/src/net/net63/codearcade/LSD/components/WorldComponent.java
+++ core/src/net/net63/codearcade/LSD/components/WorldComponent.java
@@ -1,12 +1,13 @@
 package net.net63.codearcade.LSD.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Basim on 23/06/15.
  */
-public class PlayerComponent extends Component{
+public class WorldComponent extends Component{
 
-
+    public World world;
 
 }
