0c3af7739b26233e7ea1b457f7d5b78db79a2213
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-23 21:18:40 +0100

    Added new layout and stuff for physics

--- core/src/net/net63/codearcade/LSD/utils/Constants.java
+++ core/src/net/net63/codearcade/LSD/utils/Constants.java
@@ -1,5 +1,7 @@
 package net.net63.codearcade.LSD.utils;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Game class with all the static constant variables to be used in game, 
  * not to be confused with settings which are user-defined
@@ -9,7 +11,15 @@ package net.net63.codearcade.LSD.utils;
  */
 public class Constants {
 
+    // --------------- Meta -------------------
 	public static final String LOG = "LSD";
 	public static final String TITLE = "Little Sticky Destroyer";
-	
+
+    // --------------- UI / Window ------------
+
+
+
+    // -------------- Physics -----------------
+
+    public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
 }
