ebff31c0a1751a5ec19272f89b45929fc62cac4d
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-24 21:10:48 +0100

    Added body component

---
+++ core/src/net/net63/codearcade/LSD/components/BodyComponent.java
@@ -0,0 +1,13 @@
+package net.net63.codearcade.LSD.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * Created by Basim on 24/06/15.
+ */
+public class BodyComponent extends Component{
+
+    public Body body;
+
+}
