014af05f98d3d14d4f19b681db8b377731b8e4aa
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-25 23:57:47 +0100

    Created a world manager

--- core/src/net/net63/codearcade/LSD/systems/WorldSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/WorldSystem.java
@@ -1,15 +1,36 @@
 package net.net63.codearcade.LSD.systems;
 
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.sun.xml.internal.fastinfoset.algorithm.BuiltInEncodingAlgorithm;
+import net.net63.codearcade.LSD.components.WorldComponent;
+import net.net63.codearcade.LSD.utils.Constants;
 
 /**
  * Created by Basim on 25/06/15.
  */
 public class WorldSystem extends IteratingSystem {
 
-    public WorldSystem() {
+    private ComponentMapper<WorldComponent> wm;
+    private float step;
 
+    public WorldSystem() {
+        super(Family.all(WorldComponent.class).get(), 1);
+
+        wm = ComponentMapper.getFor(WorldComponent.class);
+        step = 1.0f / Constants.BOX2D_FPS;
     }
 
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        WorldComponent worldComponent = wm.get(entity);
+
+        worldComponent.accumulator += deltaTime;
+
+        while (worldComponent.accumulator >= step) {
+            worldComponent.world.step(step, Constants.BOX2D_VELOCITY_ITERATIONS, Constants.BOX2D_POSITION_ITERATIONS);
+            worldComponent.accumulator -= step;
+        }
+    }
 }
