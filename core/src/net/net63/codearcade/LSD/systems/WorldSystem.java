e7a6fe04cf043a9e549a154d58c1da53e543b880
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-25 23:40:16 +0100

    Made base for world management

---
+++ core/src/net/net63/codearcade/LSD/systems/WorldSystem.java
@@ -0,0 +1,15 @@
+package net.net63.codearcade.LSD.systems;
+
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.sun.xml.internal.fastinfoset.algorithm.BuiltInEncodingAlgorithm;
+
+/**
+ * Created by Basim on 25/06/15.
+ */
+public class WorldSystem extends IteratingSystem {
+
+    public WorldSystem() {
+
+    }
+
+}
