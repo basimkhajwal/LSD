3fd2e4b3033c0b6d0699b89368677d9df6c036ed
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-26 21:15:13 +0100

    Created world loading boilerplate

--- core/src/net/net63/codearcade/LSD/utils/GameWorld.java
+++ core/src/net/net63/codearcade/LSD/utils/GameWorld.java
@@ -1,8 +1,14 @@
 package net.net63.codearcade.LSD.utils;
 
+import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
+import net.net63.codearcade.LSD.components.BodyComponent;
+import net.net63.codearcade.LSD.components.PlayerComponent;
+import net.net63.codearcade.LSD.components.WorldComponent;
 
 /**
  * Created by Basim on 23/06/15.
@@ -10,18 +16,49 @@ import com.badlogic.gdx.physics.box2d.World;
 public class GameWorld {
 
     private Engine engine;
+    private World world;
 
     public GameWorld (Engine engine) {
         this.engine = engine;
     }
 
-    public Entity createPlayer() {
+    public void setup() {
 
-        Entity player = new Entity();
+        world = new World(Constants.WORLD_GRAVITY, true);
 
-        engine.addEntity(player);
-
-        return player;
+        createWorld();
+        createPlayer();
     }
 
+    public Entity createWorld() {
+        WorldComponent worldComponent = new WorldComponent();
+        worldComponent.world = world;
+
+        return createEntityFrom(worldComponent);
+    }
+
+    public Entity createPlayer() {
+
+        PlayerComponent playerComponent = new PlayerComponent();
+        BodyComponent bodyComponent = new BodyComponent();
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.fixedRotation = true;
+        bodyComponent.body = world.createBody(bodyDef);
+
+        return createEntityFrom(playerComponent, bodyComponent);
+    }
+
+    private Entity createEntityFrom(Component... components) {
+        Entity entity = new Entity();
+
+        for (Component component: components) {
+            entity.add(component);
+        }
+
+        engine.addEntity(entity);
+
+        return entity;
+    }
 }
