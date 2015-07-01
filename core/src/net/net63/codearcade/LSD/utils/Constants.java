b80c561dec2feca25d694d349311dcf7efa64f76
Basim Khajwal <basimkhajwal@gmail.com>
2015-07-01 21:05:21 +0100

    Added more constants

--- core/src/net/net63/codearcade/LSD/utils/Constants.java
+++ core/src/net/net63/codearcade/LSD/utils/Constants.java
@@ -22,6 +22,9 @@ public class Constants {
 
     // -------------- Physics -----------------
 
+    public static final float METRE_TO_PIXEL = 40f;
+    public static final float PIXEL_TO_METRE = 1 / METRE_TO_PIXEL;
+
     public static final float BOX2D_FPS = 60.0f;
     public static final int BOX2D_VELOCITY_ITERATIONS = 6;
     public static final int BOX2D_POSITION_ITERATIONS = 2;
