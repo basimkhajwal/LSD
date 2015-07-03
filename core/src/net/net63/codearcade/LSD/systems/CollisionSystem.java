b97e0bc0d4a861a064854a771ebb3aef6dbd71eb
Basim Khajwal <basimkhajwal@gmail.com>
2015-07-03 23:12:45 +0100

    Some physics test code

--- core/src/net/net63/codearcade/LSD/systems/CollisionSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/CollisionSystem.java
@@ -1,10 +1,8 @@
 package net.net63.codearcade.LSD.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.physics.box2d.*;
 import net.net63.codearcade.LSD.utils.Constants;
 
 /**
@@ -24,21 +22,38 @@ public class CollisionSystem extends EntitySystem implements ContactListener {
 
     @Override
     public void beginContact(Contact contact) {
+        Gdx.app.log("Collision", "Started");
 
+        Body a = contact.getFixtureA().getBody();
+        Body b = contact.getFixtureB().getBody();
+
+        Body dynBody = null;
+
+        if (a.getType() == BodyDef.BodyType.DynamicBody) {
+            dynBody = a;
+        }
+
+        if (b.getType() == BodyDef.BodyType.DynamicBody) {
+            dynBody = b;
+        }
+
+        if (dynBody != null) {
+            dynBody.applyForceToCenter(0, 100, true);
+        }
     }
 
     @Override
     public void endContact(Contact contact) {
-
+        Gdx.app.log("Collision", "Ended");
     }
 
     @Override
     public void preSolve(Contact contact, Manifold oldManifold) {
-
+        Gdx.app.log("Collision", "Pre Solve");
     }
 
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
-
+        Gdx.app.log("Collision", "Post Solve");
     }
 }
