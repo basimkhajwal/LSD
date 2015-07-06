140ad1c6b5a077fd4c0d9c73a4e221cdc1d29d86
Basim Khajwal <basimkhajwal@gmail.com>
2015-07-06 23:00:01 +0100

    Did some more stuff

--- core/src/net/net63/codearcade/LSD/systems/WorldSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/WorldSystem.java
@@ -4,13 +4,15 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.utils.Disposable;
+import com.sun.media.jfxmediaimpl.MediaDisposer;
 import net.net63.codearcade.LSD.components.WorldComponent;
 import net.net63.codearcade.LSD.utils.Constants;
 
 /**
  * Created by Basim on 25/06/15.
  */
-public class WorldSystem extends IteratingSystem {
+public class WorldSystem extends IteratingSystem implements Disposable{
 
     private ComponentMapper<WorldComponent> worldMapper;
     private float step;
@@ -33,5 +35,10 @@ public class WorldSystem extends IteratingSystem {
             worldComponent.world.step(step, Constants.BOX2D_VELOCITY_ITERATIONS, Constants.BOX2D_POSITION_ITERATIONS);
             worldComponent.accumulator -= step;
         }
+    }
+
+    @Override
+    public void dispose() {
+        
     }
 }
