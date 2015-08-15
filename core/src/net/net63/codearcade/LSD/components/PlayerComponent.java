69dad0823943dc71ff9c1497709f9f068dcf9e71
Basim Khajwal <basimkhajwal@gmail.com>
2015-08-15 19:43:49 +0100

    Added some new states

--- core/src/net/net63/codearcade/LSD/components/PlayerComponent.java
+++ core/src/net/net63/codearcade/LSD/components/PlayerComponent.java
@@ -1,6 +1,7 @@
 package net.net63.codearcade.LSD.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Basim on 23/06/15.
@@ -9,6 +10,11 @@ public class PlayerComponent extends Component{
 
     public static final int STATE_STILL     = 0;
     public static final int STATE_AIMING    = 1;
-    public static final int STATE_JUMPING   = 2;
+    public static final int STATE_FIRING    = 2;
+    public static final int STATE_JUMPING   = 3;
+
+    public Vector2 aimPosition;
+    public Vector2 launchImpulse;
+    public Vector2[] trajectoryPoints;
 
 }
