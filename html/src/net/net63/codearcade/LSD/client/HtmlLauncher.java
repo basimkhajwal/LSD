353fcb6fe61b2df44488bb235db67e08c1435df4
Basim Khajwal <basimkhajwal@gmail.com>
2015-08-19 11:45:23 +0100

    Increased screen size for html project

--- html/src/net/net63/codearcade/LSD/client/HtmlLauncher.java
+++ html/src/net/net63/codearcade/LSD/client/HtmlLauncher.java
@@ -4,12 +4,15 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import net.net63.codearcade.LSD.LSD;
+import net.net63.codearcade.LSD.utils.Constants;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(
+                        Constants.DEFAULT_SCREEN_WIDTH,
+                        Constants.DEFAULT_SCREEN_HEIGHT);
         }
 
         @Override
