5f636b82ffff2a81f65fed978d1440c9ba8fd953
Basim Khajwal <basimkhajwal@gmail.com>
2015-08-31 22:27:22 +0100

    Did some changes

--- core/src/net/net63/codearcade/LSD/components/AnimationComponent.java
+++ core/src/net/net63/codearcade/LSD/components/AnimationComponent.java
@@ -2,12 +2,13 @@ package net.net63.codearcade.LSD.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.utils.ArrayMap;
 
 /**
  * Created by Basim on 31/08/15.
  */
 public class AnimationComponent extends Component {
 
-    public Animation animation;
+    public ArrayMap<Integer, Animation> animations = new ArrayMap<Integer, Animation>();
 
 }
