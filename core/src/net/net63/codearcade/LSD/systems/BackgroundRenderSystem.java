d5cda1222bff0ecdace5c9adc865815eef75dd0e
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-03 20:17:10 +0100

    Added class variables and disposability

--- core/src/net/net63/codearcade/LSD/systems/BackgroundRenderSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/BackgroundRenderSystem.java
@@ -1,21 +1,35 @@
 package net.net63.codearcade.LSD.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
-import com.badlogic.ashley.core.Family;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
 import net.net63.codearcade.LSD.utils.Constants;
-import sun.font.SunFontManager;
 
 /**
  * Created by Basim on 03/09/15.
  */
-public class BackgroundRenderSystem extends EntitySystem {
+public class BackgroundRenderSystem extends EntitySystem implements Disposable {
+
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
 
     public BackgroundRenderSystem() {
         super(Constants.SYSTEM_PRIORITIES.BACKGROUND_RENDER);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false);
+
+        batch = new SpriteBatch();
     }
 
     @Override
     public void update(float deltaTime) {
 
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
     }
 }
