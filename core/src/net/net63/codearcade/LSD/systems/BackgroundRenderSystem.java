dbaca763e2bed65bea69fa33db1fa5f16dd7c500
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-03 20:19:30 +0100

    Added background logic

--- core/src/net/net63/codearcade/LSD/systems/BackgroundRenderSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/BackgroundRenderSystem.java
@@ -2,8 +2,10 @@ package net.net63.codearcade.LSD.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import net.net63.codearcade.LSD.utils.Assets;
 import net.net63.codearcade.LSD.utils.Constants;
 
 /**
@@ -14,8 +16,12 @@ public class BackgroundRenderSystem extends EntitySystem implements Disposable {
     private OrthographicCamera camera;
     private SpriteBatch batch;
 
+    private Texture backgroundTexture;
+
     public BackgroundRenderSystem() {
         super(Constants.SYSTEM_PRIORITIES.BACKGROUND_RENDER);
+
+        backgroundTexture = Assets.getAsset(Assets.Images.BACKGROUND, Texture.class);
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false);
@@ -25,7 +31,10 @@ public class BackgroundRenderSystem extends EntitySystem implements Disposable {
 
     @Override
     public void update(float deltaTime) {
-
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, camera.viewportWidth, camera.viewportHeight);
+        batch.end();
     }
 
     @Override
