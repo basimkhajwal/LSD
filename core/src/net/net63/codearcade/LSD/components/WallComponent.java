a4bdfc770874595627246c3dd3f679d90a487997
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-20 11:09:34 +0100

    Created wall component

---
+++ core/src/net/net63/codearcade/LSD/components/WallComponent.java
@@ -0,0 +1,10 @@
+package net.net63.codearcade.LSD.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by Basim on 20/09/15.
+ */
+public class WallComponent implements Component{
+
+}
