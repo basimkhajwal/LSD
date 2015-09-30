cfcd3563dafb6296019a3b7fc42d3cbbf575b561
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-30 20:09:30 +0100

    Added game over and level complete transition screens

---
+++ core/src/net/net63/codearcade/LSD/screens/GameOverScreen.java
@@ -0,0 +1,19 @@
+package net.net63.codearcade.LSD.screens;
+
+import net.net63.codearcade.LSD.LSD;
+
+/**
+ * Created by Basim on 30/09/15.
+ */
+public class GameOverScreen extends AbstractScreen {
+
+    private LSD game;
+
+    public GameOverScreen(LSD game) {
+        super(game);
+
+        this.game = game;
+    }
+
+
+}
---
+++ core/src/net/net63/codearcade/LSD/screens/LevelCompleteScreen.java
@@ -0,0 +1,19 @@
+package net.net63.codearcade.LSD.screens;
+
+import net.net63.codearcade.LSD.LSD;
+
+/**
+ * Created by Basim on 30/09/15.
+ */
+public class LevelCompleteScreen extends AbstractScreen {
+
+    private LSD game;
+
+    public LevelCompleteScreen(LSD game) {
+        super(game);
+
+        this.game = game;
+    }
+
+
+}
