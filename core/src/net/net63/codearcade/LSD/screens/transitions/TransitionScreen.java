27be056f6203e6548ba5dfa0cfba38f075dc5311
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-30 20:25:25 +0100

    Started creating a transition screen that manages transitions

--- core/src/net/net63/codearcade/LSD/screens/transitions/TransitionScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/transitions/TransitionScreen.java
@@ -8,10 +8,35 @@ import net.net63.codearcade.LSD.screens.AbstractScreen;
  */
 public abstract class TransitionScreen extends AbstractScreen {
 
+    private LSD game;
 
+    private AbstractScreen previousScreen;
 
-    public TransitionScreen(LSD game) {
+    private boolean drawOverlay = false;
+
+    public TransitionScreen(LSD game, AbstractScreen previousScreen) {
         super(game);
+
+        this.previousScreen = previousScreen;
+        this.game = game;
     }
 
+    public void switchOver(AbstractScreen newScreen) {
+        previousScreen.dispose();
+        this.dispose();
+        game.setScreen(newScreen);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        previousScreen.resize(width, height);
+    }
+
+    @Override
+    public void render(float deltaTime) {
+        super.render(deltaTime);
+
+        previousScreen.render(deltaTime);
+    }
 }
