55c1bbf1b657b68be30b397fd9c82aa0b9fb35e9
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-30 22:10:26 +0100

    Realized I didn't need an abstract super class

--- core/src/net/net63/codearcade/LSD/screens/transitions/GameOverScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/transitions/GameOverScreen.java
@@ -1,5 +1,6 @@
 package net.net63.codearcade.LSD.screens.transitions;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import net.net63.codearcade.LSD.LSD;
 import net.net63.codearcade.LSD.screens.AbstractScreen;
 
@@ -10,11 +11,41 @@ public class GameOverScreen extends AbstractScreen {
 
     private LSD game;
 
+    private Stage stage;
+
     public GameOverScreen(LSD game) {
         super(game);
 
         this.game = game;
+
+        stage = new Stage();
     }
 
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
 
+    @Override
+    public void show() {
+        super.show();
+
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
 }
