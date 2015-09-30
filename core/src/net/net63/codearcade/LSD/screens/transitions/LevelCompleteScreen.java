a96055a25d89b068777bd483c86e84c4717920e6
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-30 20:11:44 +0100

    Created new transition package and moved stuff across, and added new abstract screen that will handle most of the boilerplate

--- core/src/net/net63/codearcade/LSD/screens/transitions/LevelCompleteScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/transitions/LevelCompleteScreen.java
@@ -1,6 +1,7 @@
-package net.net63.codearcade.LSD.screens;
+package net.net63.codearcade.LSD.screens.transitions;
 
 import net.net63.codearcade.LSD.LSD;
+import net.net63.codearcade.LSD.screens.AbstractScreen;
 
 /**
  * Created by Basim on 30/09/15.
---
+++ core/src/net/net63/codearcade/LSD/screens/transitions/TransitionScreen.java
@@ -0,0 +1,17 @@
+package net.net63.codearcade.LSD.screens.transitions;
+
+import net.net63.codearcade.LSD.LSD;
+import net.net63.codearcade.LSD.screens.AbstractScreen;
+
+/**
+ * Created by Basim on 30/09/15.
+ */
+public abstract class TransitionScreen extends AbstractScreen {
+
+
+
+    public TransitionScreen(LSD game) {
+        super(game);
+    }
+
+}
