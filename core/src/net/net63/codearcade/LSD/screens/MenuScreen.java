0959bda335dffea48ced1a3dbf6e7db5fca0195e
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-01 17:39:50 +0100

    Simplified menu screen code

--- core/src/net/net63/codearcade/LSD/screens/MenuScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/MenuScreen.java
@@ -2,22 +2,20 @@ package net.net63.codearcade.LSD.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import net.net63.codearcade.LSD.LSD;
 import net.net63.codearcade.LSD.utils.Assets;
 
@@ -36,35 +34,29 @@ public class MenuScreen extends AbstractScreen{
     private static final Color BOTTOM_TITLE = new Color(150 / 255.0f, 30 / 255.0f, 0f, 1f);
 
 	private Stage stage;
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
+    private Image backgroundImage;
 	
-	private Image backgroundImage;
-	private ImageButton playButton;
-	private Label topTitle;
-    private Label bottomTitle;
-	
-	private boolean changing;
+	private boolean changingScreen;
 	
 	public MenuScreen(LSD game) {
 		super(game);
-		this.clear = new Color(1, 0, 0, 1);
 		
-		changing = false;
+		changingScreen = false;
 		
 		stage = new Stage(new ExtendViewport(840, 480));
-		batch = new SpriteBatch();
-		camera = new OrthographicCamera();
-		
 		Gdx.input.setInputProcessor(stage);
 		
-		topTitle = new Label("Little Sticky",
-				new LabelStyle(Assets.getFont(Assets.Fonts.DEFAULT, Assets.FontSizes.HUNDRED), TOP_TITLE));
-		topTitle.setAlignment(Align.center);
-		topTitle.setPosition((840 - topTitle.getWidth()) / 2, 350);
+		setupUI();
+	}
 
-        bottomTitle = new Label("Destroyer",
-                new LabelStyle(Assets.getFont(Assets.Fonts.DEFAULT, Assets.FontSizes.HUNDRED), BOTTOM_TITLE));
+    private void setupUI() {
+        Label topTitle = new Label("Little Sticky",
+                new Label.LabelStyle(Assets.getFont(Assets.Fonts.DEFAULT, Assets.FontSizes.HUNDRED), TOP_TITLE));
+        topTitle.setAlignment(Align.center);
+        topTitle.setPosition((840 - topTitle.getWidth()) / 2, 350);
+
+        Label bottomTitle = new Label("Destroyer",
+                new Label.LabelStyle(Assets.getFont(Assets.Fonts.DEFAULT, Assets.FontSizes.HUNDRED), BOTTOM_TITLE));
         bottomTitle.setAlignment(Align.center);
         bottomTitle.setPosition((840 - bottomTitle.getWidth()) / 2, topTitle.getY() - bottomTitle.getHeight());
 
@@ -72,74 +64,62 @@ public class MenuScreen extends AbstractScreen{
         TextureRegionDrawable btnDown = new TextureRegionDrawable(new TextureRegion(Assets.getAsset(Assets.Images.PLAY_BUTTON_DOWN, Texture.class)));
         TextureRegionDrawable btnChecked = new TextureRegionDrawable(new TextureRegion(Assets.getAsset(Assets.Images.PLAY_BUTTON_HOVER, Texture.class)));
 
-		playButton = new ImageButton(btn, btnDown, btnChecked);
-		playButton.setSize(140f, 140f);
-		playButton.setPosition((840 - playButton.getWidth()) / 2.0f, 60);
-		playButton.addListener(new ClickListener() {
-			
-			@Override
-			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-				super.touchUp(event, x, y, pointer, button);
+        ImageButton playButton = new ImageButton(btn, btnDown, btnChecked);
+        playButton.setSize(140f, 140f);
+        playButton.setPosition((840 - playButton.getWidth()) / 2.0f, 60);
+        playButton.addListener(new ClickListener() {
 
-                if (this.inTapSquare()) {
-                    changing = true;
-                }
-			}
-		});
-		
-		setupBackground();
-	}
-	
-	private void setupBackground() {
-		Texture backgroundTexture = Assets.getAsset(Assets.Images.BACKGROUND, Texture.class);
-		backgroundTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		backgroundImage = new Image(backgroundTexture);
-		backgroundImage.setScaling(Scaling.stretch);
-	}
-	
-	@Override
-	public void show() {
-		super.show();
-		
-		stage.addActor(topTitle);
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+
+                if (this.inTapSquare()) changingScreen = true;
+            }
+        });
+
+        Texture backgroundTexture = Assets.getAsset(Assets.Images.BACKGROUND, Texture.class);
+        backgroundTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        backgroundImage = new Image(backgroundTexture);
+        backgroundImage.setScaling(Scaling.stretch);
+
+        stage.addActor(backgroundImage);
+        stage.addActor(topTitle);
         stage.addActor(bottomTitle);
-		stage.addActor(playButton);
-	}
+        stage.addActor(playButton);
+    }
 	
 	@Override
 	public void resize(int width, int height) {
 		super.resize(width, height);
-		
-		stage.getViewport().update(width, height);
-		camera.setToOrtho(false, width, height);
-		
-		backgroundImage.setPosition(0, 0);
-		backgroundImage.setSize(width, height);
+
+        Viewport viewport = stage.getViewport();
+		viewport.update(width, height);
+
+        Vector2 zero = new Vector2(0, height - 1);
+        viewport.unproject(zero);
+
+		backgroundImage.setPosition(zero.x, zero.y);
+		backgroundImage.setSize(viewport.getWorldWidth(), viewport.getWorldHeight());
 	}
 	
 	@Override
 	public void render(float delta) {
 		super.render(delta);
-        
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		backgroundImage.draw(batch, 1.0f);
-		batch.end();
-		
-		stage.getViewport().apply();
+
 		stage.act(delta);
 		stage.draw();
 		
-		if (changing) {
-			game.setScreen(new GameScreen(game));
-		}
+		if (changingScreen) {
+            dispose();
+            game.setScreen(new GameScreen(game, 0));
+        }
 	}
 	
 	@Override
 	public void dispose() {
 		super.dispose();
-		
-		batch.dispose();
+
 		stage.dispose();
 	}
 	
