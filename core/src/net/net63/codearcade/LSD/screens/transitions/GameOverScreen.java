9b7501a33223376e0de8762a639d9785e5c38ad3
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-10 21:34:58 +0100

    Put next level into place

--- core/src/net/net63/codearcade/LSD/screens/transitions/GameOverScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/transitions/GameOverScreen.java
@@ -1,5 +1,6 @@
 package net.net63.codearcade.LSD.screens.transitions;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -7,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -39,6 +41,7 @@ public class GameOverScreen extends AbstractScreen {
         this.previousGame = previousGame;
 
         stage = new Stage(new ExtendViewport(Constants.DEFAULT_SCREEN_WIDTH, Constants.DEFAULT_SCREEN_HEIGHT));
+        Gdx.input.setInputProcessor(stage);
 
         Pixmap overlay = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
         overlay.setColor(0, 0, 0, 0.5f);
@@ -56,19 +59,23 @@ public class GameOverScreen extends AbstractScreen {
 
         BitmapFont fontFifty = Assets.getFont(Assets.Fonts.DEFAULT, Assets.FontSizes.FIFTY);
 
-        int width = Constants.DEFAULT_SCREEN_WIDTH;
-        int height = Constants.DEFAULT_SCREEN_HEIGHT;
-
         Image backingImage = new Image(Assets.getAsset(Assets.Images.TRANSITION_BACKGROUND, Texture.class));
         backingImage.setSize(400, 320);
-        backingImage.setPosition(200, 140);
+        backingImage.setPosition(200, 200);
 
         Label gameOverLabel = new Label("Game Over", new Label.LabelStyle(fontFifty, Color.ORANGE));
-        gameOverLabel.setPosition((width - gameOverLabel.getWidth()) / 2, (4 * height) / 5);
+        gameOverLabel.setPosition((800 - gameOverLabel.getWidth()) / 2, 440);
+
+        ImageButton nextLevelButton = Assets.createButton(Assets.Buttons.NEXT_LEVEL);
+        nextLevelButton.setSize(120, 90);
+        nextLevelButton.setPosition(480, 90);
+
+        
 
         stage.addActor(overlay);
         stage.addActor(backingImage);
         stage.addActor(gameOverLabel);
+        stage.addActor(nextLevelButton);
     }
 
     @Override
