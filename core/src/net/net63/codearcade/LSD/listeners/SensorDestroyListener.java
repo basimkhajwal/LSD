f49dd3473a4a32d8e28d6031d485d032cdb3778a
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-13 20:37:09 +0100

    Begun writing particle logic

--- core/src/net/net63/codearcade/LSD/listeners/SensorDestroyListener.java
+++ core/src/net/net63/codearcade/LSD/listeners/SensorDestroyListener.java
@@ -3,8 +3,10 @@ package net.net63.codearcade.LSD.listeners;
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntityListener;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import net.net63.codearcade.LSD.components.BodyComponent;
 import net.net63.codearcade.LSD.components.SensorComponent;
 
@@ -13,11 +15,17 @@ import net.net63.codearcade.LSD.components.SensorComponent;
  */
 public class SensorDestroyListener implements EntityListener {
 
+    private static final float PARTICLE_SIZE = 0.2f;
+
     private ComponentMapper<SensorComponent> sensorMapper;
     private ComponentMapper<BodyComponent> bodyMapper;
 
-    public SensorDestroyListener() {
+    private World world;
+
+    public SensorDestroyListener(World world) {
         super();
+
+        this.world = world;
 
         sensorMapper = ComponentMapper.getFor(SensorComponent.class);
         bodyMapper = ComponentMapper.getFor(BodyComponent.class);
@@ -32,7 +40,25 @@ public class SensorDestroyListener implements EntityListener {
             Body body = bodyMapper.get(entity).body;
             PolygonShape shape = (PolygonShape) body.getFixtureList().first().getShape();
 
-            
+            Vector2 dimensions = new Vector2();
+            shape.getVertex(2, dimensions);
+
+            Vector2 bottomLeft = body.getPosition().cpy().sub(dimensions);
+
+            dimensions.scl(2);
+
+            int particlesAcross = (int) (dimensions.x / PARTICLE_SIZE);
+            int particlesDown = (int) (dimensions.y / PARTICLE_SIZE);
+
+            for (int j = 0; j < particlesDown; j++) {
+                for (int i = 0; i < particlesAcross; i++) {
+
+                }
+            }
         }
+    }
+
+    private Body createParticle(float x, float y) {
+        
     }
 }
