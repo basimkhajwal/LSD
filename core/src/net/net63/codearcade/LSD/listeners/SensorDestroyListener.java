cae0a30fb0b5b9fdf2a28a89195dd1d61bd39722
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-14 19:15:19 +0100

    Added particle effect creation code

--- core/src/net/net63/codearcade/LSD/listeners/SensorDestroyListener.java
+++ core/src/net/net63/codearcade/LSD/listeners/SensorDestroyListener.java
@@ -1,13 +1,13 @@
 package net.net63.codearcade.LSD.listeners;
 
 import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntityListener;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 import net.net63.codearcade.LSD.components.BodyComponent;
+import net.net63.codearcade.LSD.components.ParticleComponent;
 import net.net63.codearcade.LSD.components.SensorComponent;
 
 /**
@@ -21,10 +21,12 @@ public class SensorDestroyListener implements EntityListener {
     private ComponentMapper<BodyComponent> bodyMapper;
 
     private World world;
+    private Engine engine;
 
-    public SensorDestroyListener(World world) {
+    public SensorDestroyListener(Engine engine, World world) {
         super();
 
+        this.engine = engine;
         this.world = world;
 
         sensorMapper = ComponentMapper.getFor(SensorComponent.class);
@@ -50,15 +52,35 @@ public class SensorDestroyListener implements EntityListener {
             int particlesAcross = (int) (dimensions.x / PARTICLE_SIZE);
             int particlesDown = (int) (dimensions.y / PARTICLE_SIZE);
 
+            ParticleComponent particleComponent = new ParticleComponent();
+
             for (int j = 0; j < particlesDown; j++) {
                 for (int i = 0; i < particlesAcross; i++) {
-
+                    particleComponent.particles.add(createParticle(bottomLeft.x + i * PARTICLE_SIZE, bottomLeft.y + j * PARTICLE_SIZE));
                 }
             }
+
+            Entity particleEntity = new Entity();
+            particleEntity.add(particleComponent);
+            engine.addEntity(particleEntity);
         }
     }
 
     private Body createParticle(float x, float y) {
-        
+        BodyDef bodyDef = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+
+        bodyDef.position.set(x, y);
+        bodyDef.fixedRotation = false;
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(PARTICLE_SIZE / 2, PARTICLE_SIZE / 2);
+        fixtureDef.shape = shape;
+
+        Body body = world.createBody(bodyDef);
+        body.createFixture(fixtureDef);
+
+        return body;
     }
 }
