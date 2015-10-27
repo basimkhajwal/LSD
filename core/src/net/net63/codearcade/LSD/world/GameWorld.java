628dbf91af328f751c3eea37b2715e020fe1ede9
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-27 15:29:14 +0000

    More comments

--- core/src/net/net63/codearcade/LSD/world/GameWorld.java
+++ core/src/net/net63/codearcade/LSD/world/GameWorld.java
@@ -19,6 +19,10 @@ import net.net63.codearcade.LSD.systems.*;
 import net.net63.codearcade.LSD.utils.Constants;
 
 /**
+ * The main game world handles and controls the game events
+ * and is responsible for rendering and updating the logic
+ * of the game but maintains no GUI of its own
+ *
  * Created by Basim on 23/06/15.
  */
 public class GameWorld implements Disposable, EntityListener {
@@ -38,41 +42,61 @@ public class GameWorld implements Disposable, EntityListener {
 
     private boolean logicPaused = false;
 
+    /**
+     * Create a new game with the given map
+     *
+     * @param map The level map
+     */
     public GameWorld (TiledMap map) {
-        engine = new Engine();
-        world = new World(Constants.WORLD_GRAVITY, true);
-
+        //Setup the camera and viewport
         gameCamera = new OrthographicCamera();
         viewport = new ExtendViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, gameCamera);
 
+        //Component mappers for fast component retrieval
         playerMapper = ComponentMapper.getFor(PlayerComponent.class);
         stateMapper = ComponentMapper.getFor(StateComponent.class);
         sensorMapper = ComponentMapper.getFor(SensorComponent.class);
 
-        resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        //Setup the world to the given map
         setup(map);
     }
 
-    private void setup(TiledMap newMap) {
+    private void setup(TiledMap gameMap) {
+        //Initialise the entity engine and Box2d world
+        engine = new Engine();
         world = new World(Constants.WORLD_GRAVITY, true);
+
+        //Level descriptor handles score, achievements etc.
         levelDescriptor = new LevelDescriptor();
 
+        //Add entity systems for logic/rendering and listeners
         addSystems();
         addListeners();
 
+        //Add all the elements from the tiled map
         WorldBuilder.setup(engine, world, levelDescriptor);
-        WorldBuilder.loadFromMap(newMap);
+        WorldBuilder.loadFromMap(gameMap);
 
+        //Maintain a pointer to the player entity since it is commonly used
         player = engine.getEntitiesFor(Family.all(PlayerComponent.class).get()).first();
     }
 
+    /**
+     * Resize this world to the new screen dimensions
+     *
+     * @param w Screen width
+     * @param h Screen height
+     */
     public void resize(int w, int h) {
+        //Update the viewport
         viewport.update(w, h, true);
-        CameraMovementSystem cam = engine.getSystem(CameraMovementSystem.class);
 
+        //If systems are loaded then centralise it to the player
+        CameraMovementSystem cam = engine.getSystem(CameraMovementSystem.class);
         if (cam != null) cam.forceUpdate();
     }
 
+    //Temporary debug to easily move around the world
     private void debugInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) gameCamera.translate(0, 1);
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) gameCamera.translate(0, -1);
@@ -86,31 +110,54 @@ public class GameWorld implements Disposable, EntityListener {
     public void update(float delta) {
         if (!logicPaused) debugInput();
 
+        //Set the viewport and appropriate camera
         viewport.apply();
         gameCamera.update();
 
+        //Call all the logic
         engine.update(delta);
+
+        //Reset the camera if it was moved when shaking
         engine.getSystem(CameraShakeSystem.class).restoreCamera();
     }
 
+    /**
+     * Aim the player to a certain point as
+     * the highest point on it's trajectory
+     *
+     * @param x The x-position in screen space
+     * @param y The y-position in screen space
+     */
     public void aimPlayer(int x, int y) {
         StateComponent state = stateMapper.get(player);
 
+        //Check if the player is still or already aiming, otherwise don't aim
         if (state.get() == PlayerComponent.STATE_STILL || state.get() == PlayerComponent.STATE_AIMING) {
+            //Convert to world space
             Vector2 worldPos = new Vector2(x, y);
             viewport.unproject(worldPos);
 
+            //Set the aim position
             state.set(PlayerComponent.STATE_AIMING);
             playerMapper.get(player).aimPosition = worldPos;
         }
     }
 
+    /**
+     *  Fire the player if it is already aiming
+     */
     public void launchPlayer() {
+        //Fire the player if the player state is aiming
         if (stateMapper.get(player).get() == PlayerComponent.STATE_AIMING) {
             stateMapper.get(player).set(PlayerComponent.STATE_FIRING);
         }
     }
 
+    /**
+     * Pause logic so when update is called
+     * the game won't continue but will still
+     * be rendered
+     */
     public void pauseLogic() {
         if (logicPaused) return;
 
@@ -124,6 +171,9 @@ public class GameWorld implements Disposable, EntityListener {
         logicPaused = true;
     }
 
+    /**
+     * Resume logic if the game was previously paused
+     */
     public void resumeLogic() {
         if (!logicPaused) return;
 
@@ -135,6 +185,7 @@ public class GameWorld implements Disposable, EntityListener {
         logicPaused = false;
     }
 
+    //Add the entity systems that contain the bulk of the logic
     private void addSystems() {
         engine.addSystem(new WorldSystem());
         engine.addSystem(new PlayerAimSystem());
@@ -152,6 +203,7 @@ public class GameWorld implements Disposable, EntityListener {
         engine.addSystem(new EffectRenderSystem(gameCamera));
     }
 
+    //Add listeners for physics and entity events
     private void addListeners() {
         engine.addEntityListener(0, new SensorDestroyListener(engine, world));
         engine.addEntityListener(1, this);
--- core/src/net/net63/codearcade/LSD/world/LevelDescriptor.java
+++ core/src/net/net63/codearcade/LSD/world/LevelDescriptor.java
@@ -3,6 +3,10 @@ package net.net63.codearcade.LSD.world;
 import com.badlogic.gdx.math.Rectangle;
 
 /**
+ * Level descriptor to hold game related
+ * information such as score, achievements
+ * etc.
+ *
  * Created by Basim on 12/09/15.
  */
 public class LevelDescriptor {
