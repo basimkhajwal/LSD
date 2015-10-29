21218b2aa8a9b5d33623ab752159d9fdd5a42d52
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-29 12:37:59 +0000

    Changed rendering to drawing the image

--- core/src/net/net63/codearcade/LSD/systems/EffectRenderSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/EffectRenderSystem.java
@@ -2,13 +2,14 @@ package net.net63.codearcade.LSD.systems;
 
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.utils.ImmutableArray;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 import net.net63.codearcade.LSD.components.PlayerComponent;
 import net.net63.codearcade.LSD.components.StateComponent;
+import net.net63.codearcade.LSD.utils.Assets;
 import net.net63.codearcade.LSD.utils.Constants;
 
 /**
@@ -17,7 +18,8 @@ import net.net63.codearcade.LSD.utils.Constants;
 public class EffectRenderSystem extends EntitySystem implements Disposable{
 
     private OrthographicCamera gameCamera;
-    private ShapeRenderer shapeRenderer;
+    private SpriteBatch batch;
+    private Texture trajectoryImage;
 
     private ComponentMapper<PlayerComponent> playerMapper;
     private ComponentMapper<StateComponent> stateMapper;
@@ -28,7 +30,9 @@ public class EffectRenderSystem extends EntitySystem implements Disposable{
         super(Constants.SYSTEM_PRIORITIES.EFFECT_RENDER);
 
         this.gameCamera = gameCamera;
-        shapeRenderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+
+        trajectoryImage = Assets.getAsset(Assets.Images.TRAJECTORY, Texture.class);
 
         stateMapper = ComponentMapper.getFor(StateComponent.class);
         playerMapper = ComponentMapper.getFor(PlayerComponent.class);
@@ -48,22 +52,21 @@ public class EffectRenderSystem extends EntitySystem implements Disposable{
         if (stateMapper.get(player).get() == PlayerComponent.STATE_AIMING) {
             Vector2[] trajectories = playerMapper.get(player).trajectoryPoints;
 
-            shapeRenderer.setProjectionMatrix(gameCamera.combined);
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-            shapeRenderer.setColor(Color.RED);
+            batch.setProjectionMatrix(gameCamera.combined);
+            batch.begin();
 
             for (Vector2 point: trajectories) {
-                shapeRenderer.rect(point.x, point.y, 0.1f, 0.1f);
+                batch.draw(trajectoryImage, point.x, point.y, 0.1f, 0.1f);
             }
 
-            shapeRenderer.end();
+            batch.end();
         }
 
     }
 
     @Override
     public void dispose() {
-        shapeRenderer.dispose();
+        batch.dispose();
     }
 
 }
