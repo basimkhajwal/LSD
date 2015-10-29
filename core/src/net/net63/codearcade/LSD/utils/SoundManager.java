e0441f5159987132bfe018b9370d7fb60ab4bfab
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-29 14:23:02 +0000

    Added sound loading

--- core/src/net/net63/codearcade/LSD/utils/SoundManager.java
+++ core/src/net/net63/codearcade/LSD/utils/SoundManager.java
@@ -2,6 +2,8 @@ package net.net63.codearcade.LSD.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.ArrayMap;
 
 /**
  * Created by Basim on 29/10/15.
@@ -17,10 +19,15 @@ public class SoundManager {
         public static final String EXPLOSION = "sounds/explosion.wav";
 
     }
+    private static final String[] _Sounds = {Sounds.PLAYER_DEATH, Sounds.EXPLOSION };
+    private static final ArrayMap<String, Sound> sounds = new ArrayMap<String, Sound>();
 
     public static void loadAll() {
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal(BACKGROUND_MUSIC));
 
+        for (String soundFile: _Sounds) {
+            sounds.put(soundFile, Gdx.audio.newSound(Gdx.files.internal(soundFile)));
+        }
     }
 
     public static void playMusic() {
@@ -32,7 +39,9 @@ public class SoundManager {
         backgroundMusic.pause();
     }
 
-    public static void playSound(String sound) {
+    public static int playSound(String sound) {
+
+        int soundId = sounds.get(sound).play();
 
     }
 
