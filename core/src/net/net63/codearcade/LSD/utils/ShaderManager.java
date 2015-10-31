fe5a01d4982b183f53a67d3c9d8b7fcfebf3fab1
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-31 20:00:39 +0000

    Created a shader manager to handle loading shaders

---
+++ core/src/net/net63/codearcade/LSD/utils/ShaderManager.java
@@ -0,0 +1,40 @@
+package net.net63.codearcade.LSD.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.ArrayMap;
+
+/**
+ * Created by Basim on 31/10/15.
+ */
+public class ShaderManager {
+
+    public static class Shaders {
+
+        public static final String MENU = "shaders/menu";
+
+    }
+    private static final String[] _Shaders = { Shaders.MENU };
+    private static final ArrayMap<String, String> shaderCache = new ArrayMap<String, String>();
+
+    public static void loadAll() {
+
+        for (String shaderName: _Shaders) {
+
+            String vertex = shaderName + "_vertex.glsl";
+            String fragment = shaderName + "_fragment.glsl";
+
+            shaderCache.put(vertex, Gdx.files.internal(vertex).readString());
+            shaderCache.put(fragment, Gdx.files.internal(fragment).readString());
+        }
+
+    }
+
+    public static String getVertexShader(String shaderName) {
+        return shaderCache.get(shaderName + "_vertex.glsl");
+    }
+
+    public static String getFragmentShader(String shaderName) {
+        return shaderCache.get(shaderName + "_fragment.glsl");
+    }
+
+}
