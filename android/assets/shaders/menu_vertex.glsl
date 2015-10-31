d13b37eaabdb6945b71a764cde5970c904c86d23
Basim Khajwal <basimkhajwal@gmail.com>
2015-10-31 22:32:17 +0000

    Fixed matrix rename

--- android/assets/shaders/menu_vertex.glsl
+++ android/assets/shaders/menu_vertex.glsl
@@ -2,7 +2,7 @@ attribute vec4 a_position;
 attribute vec4 a_color;
 attribute vec2 a_texCoord0;
 
-uniform mat4 u_worldView;
+uniform mat4 u_projTrans;
 
 varying vec4 v_color;
 varying vec2 v_texCoords;
@@ -11,5 +11,5 @@ void main() {
     v_color = vec4(1, 1, 1, 1);
     v_texCoords = a_texCoord0;
 
-    gl_Position =  u_worldView * a_position;
+    gl_Position =  u_projTrans * a_position;
 }
