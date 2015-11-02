16436161a210abc772b8d3858ac40af0d4278e4d
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-02 18:08:48 +0000

    Improved menu shader

--- android/assets/shaders/menu_fragment.glsl
+++ android/assets/shaders/menu_fragment.glsl
@@ -14,15 +14,20 @@ float rand(vec2 co){
 
 void main() {
     vec2 stepX = vec2(invScreenSize.x, 0);
+    vec2 stepY = vec2(0, invScreenSize.y);
 
     vec4 left = texture2D(u_texture, v_texCoords - stepX);
     vec4 right = texture2D(u_texture, v_texCoords + stepX);
+
+    vec4 top = texture2D(u_texture, v_texCoords - stepY);
+    vec4 bottom = texture2D(u_texture, v_texCoords + stepY);
+
     vec4 middle = texture2D(u_texture, v_texCoords);
 
-    float ratio = 1.0 / 3.0;
+    float ratio = 1.0 / 5.0;
 
     vec4 noise = vec4(rand(v_texCoords * time), rand(v_texCoords * (time + 1.0) ), rand(v_texCoords * (time - 1.0) ), 1);
 
-    gl_FragColor = noise * 0.2 + (left + middle + right) * ratio * 0.92;
+    gl_FragColor = noise * 0.2 + (left + middle + right + top + bottom) * ratio * 0.92;
     gl_FragColor *= vec4(1,1,1,0.95);
 }
