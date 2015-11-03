4263db8f300c651742351d0190e91b7a79e4af85
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-03 17:03:23 +0000

    Added comments

--- core/src/net/net63/codearcade/LSD/screens/MenuScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/MenuScreen.java
@@ -36,46 +36,56 @@ public class MenuScreen extends AbstractScreen{
     private static final Color TOP_TITLE = new Color(220 / 255.0f, 80 / 255.0f, 0f, 1f);
     private static final Color BOTTOM_TITLE = new Color(150 / 255.0f, 30 / 255.0f, 0f, 1f);
 
+    //GUI
 	private Stage stage;
     private ImageButton playButton;
 
+    //Shader rendering
     private SpriteBatch batch;
     private OrthographicCamera camera;
+
+    //Background and shader
     private Texture backgroundTexture;
     private Vector2 backgroundSize = new Vector2();
     private ShaderProgram shaderProgram;
+
+    //Background position
     private float backgroundX = 0;
     private float time = 0;
 
-    private boolean changingScreen;
+    private boolean changingScreen = false;
 
     public MenuScreen(LSD game) {
         super(game);
 
-        changingScreen = false;
-
+        //Instantiate the batch and camera for drawing the background
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
 
+        //Load the shader and set pedantic to stop un-used uniform errors
+        shaderProgram = ShaderManager.getShader(ShaderManager.Shaders.MENU);
+        shaderProgram.pedantic = false;
+
+        //Create the stage for the GUI and the input handler
         stage = new Stage(new ExtendViewport(Constants.DEFAULT_SCREEN_WIDTH, Constants.DEFAULT_SCREEN_HEIGHT));
         Gdx.input.setInputProcessor(stage);
 
-        shaderProgram = ShaderManager.getShader(ShaderManager.Shaders.MENU);
-        shaderProgram.pedantic = false;
-        batch.setShader(shaderProgram);
 
         setupUI();
     }
 
     private void setupUI() {
+        //Top title text
         Label topTitle = GUIBuilder.createLabel("Little Sticky", 100, TOP_TITLE);
         topTitle.setAlignment(Align.center);
         topTitle.setPosition((800 - topTitle.getWidth()) / 2, 450);
 
+        //Bottom title text
         Label bottomTitle = GUIBuilder.createLabel("Destroyer", 100, BOTTOM_TITLE);
         bottomTitle.setAlignment(Align.center);
         bottomTitle.setPosition((800 - bottomTitle.getWidth()) / 2, topTitle.getY() - bottomTitle.getHeight());
 
+        //The play button to handle play-game
         playButton = GUIBuilder.createButton(Assets.Buttons.MENU_PLAY);
         playButton.setSize(140f, 140f);
         playButton.setPosition((800 - playButton.getWidth()) / 2.0f, 100);
@@ -85,14 +95,17 @@ public class MenuScreen extends AbstractScreen{
             public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
                 super.touchUp(event, x, y, pointer, button);
 
+                //If the mouse is released on the button apply the change
                 if (this.inTapSquare()) changingScreen = true;
             }
         });
 
+        //Get the background and set the shader uniform
         backgroundTexture = Assets.getAsset(Assets.Images.BACKGROUND, Texture.class);
         backgroundTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         shaderProgram.setUniformf("invScreenSize", 1.0f/backgroundTexture.getWidth(), 1.0f/backgroundTexture.getHeight());
 
+        //Add all the GUI elements
         stage.addActor(topTitle);
         stage.addActor(bottomTitle);
         stage.addActor(playButton);
@@ -102,19 +115,23 @@ public class MenuScreen extends AbstractScreen{
 	public void resize(int width, int height) {
 		super.resize(width, height);
 
+        //Update the viewport size
         Viewport viewport = stage.getViewport();
 		viewport.update(width, height);
 
+        //Reposition the viewport to the centre
         Camera stageCam = stage.getViewport().getCamera();
         stageCam.position.x = Constants.DEFAULT_SCREEN_WIDTH / 2;
         stageCam.position.y = Constants.DEFAULT_SCREEN_HEIGHT / 2;
         stageCam.update();
 
+        //Update the shader camera and resend the uniform for the screenSize
         camera.setToOrtho(false, width, height);
         shaderProgram.begin();
         shaderProgram.setUniformf("screenSize", width, height);
         shaderProgram.end();
 
+        //Reset the size and position of the background
         backgroundX = 0;
         backgroundSize.set(width, height);
 	}
@@ -123,25 +140,32 @@ public class MenuScreen extends AbstractScreen{
 	public void render(float delta) {
 		super.render(delta);
 
+        //Update the time
         time += delta;
 
+        //Move the background, reset if it passes the screen
         backgroundX += 25 * delta;
         if (backgroundX >= backgroundSize.x) {
             backgroundX = 0;
         }
 
+        //Set the shader and pass the time uniform
         batch.setShader(shaderProgram);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         shaderProgram.setUniformf("time", time);
+
+        //Draw the background twice, side by side
         batch.draw(backgroundTexture, backgroundX, 0, backgroundSize.x, backgroundSize.y);
         batch.draw(backgroundTexture, backgroundX - backgroundSize.x, 0, backgroundSize.x, backgroundSize.y);
         batch.end();
 
+        //Update and render the UI
 		stage.act(delta);
         playButton.setChecked(playButton.isOver());
 		stage.draw();
 
+        //If the button has been clicked start the game
 		if (changingScreen) {
             dispose();
             game.setScreen(new GameScreen(game, 0));
