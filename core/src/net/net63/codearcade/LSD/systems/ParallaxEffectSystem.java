67e210fc709e9fe4747ce6411c4921fb09bc983a
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-11 17:13:03 +0000

    Started parallax effect

---
+++ core/src/net/net63/codearcade/LSD/systems/ParallaxEffectSystem.java
@@ -0,0 +1,15 @@
+package net.net63.codearcade.LSD.systems;
+
+import com.badlogic.ashley.core.EntitySystem;
+
+/**
+ * Created by Basim on 11/11/15.
+ */
+public class ParallaxEffectSystem extends EntitySystem {
+
+    public ParallaxEffectSystem() {
+        
+    }
+
+
+}
