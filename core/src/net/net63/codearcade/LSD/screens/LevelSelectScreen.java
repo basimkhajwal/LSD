8a583ef3a760788e3d146217e9297a93cb5dc626
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-15 15:03:56 +0000

    Added screen stub

--- core/src/net/net63/codearcade/LSD/screens/LevelSelectScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/LevelSelectScreen.java
@@ -1,10 +1,35 @@
 package net.net63.codearcade.LSD.screens;
 
+import net.net63.codearcade.LSD.LSD;
+
 /**
  * Created by Basim on 13/11/15.
  */
 public class LevelSelectScreen extends AbstractScreen {
 
+    private LSD game;
+
+    public LevelSelectScreen(LSD game) {
+        super(game);
+
+        this.game = game;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void render(float deltaTime) {
+        super.render(deltaTime);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        
+    }
 
 
 }
