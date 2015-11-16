5d4ffa09759d0982ee76ee010d35888985b3b257
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-16 18:59:41 +0000

    Created generic renderer

--- core/src/net/net63/codearcade/LSD/utils/BackgroundRenderer.java
+++ core/src/net/net63/codearcade/LSD/utils/BackgroundRenderer.java
@@ -1,8 +1,10 @@
 package net.net63.codearcade.LSD.utils;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 
 /**
@@ -15,9 +17,19 @@ public class BackgroundRenderer implements Disposable {
     private OrthographicCamera camera;
     private SpriteBatch batch;
 
+    private Texture backgroundTexture;
+    private BackgroundRenderable renderMethod;
 
-    public BackgroundRenderer(ShaderProgram shaderProgram) {
+    private Vector2 screenSize = new Vector2();
+    private float time;
+
+    public BackgroundRenderer(ShaderProgram shaderProgram, BackgroundRenderable renderMethod) {
         this.shaderProgram = shaderProgram;
+        shaderProgram.pedantic = false;
+
+        this.renderMethod = renderMethod;
+        renderMethod.setup(this);
+        backgroundTexture = Assets.getAsset(Assets.Images.BACKGROUND, Texture.class);
 
         camera = new OrthographicCamera();
         batch = new SpriteBatch();
@@ -25,16 +37,57 @@ public class BackgroundRenderer implements Disposable {
 
     public void render(float deltaTime) {
 
+        time += deltaTime;
+
         batch.setShader(shaderProgram);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
+        shaderProgram.setUniformf("time", time);
+        renderMethod.renderBackground(batch, backgroundTexture);
 
         batch.end();
     }
 
     public void resize(int width, int height) {
-        camera.setToOrtho(false);
+        camera.setToOrtho(false, width, height);
+        screenSize.set(width, height);
+
+        shaderProgram.begin();
+        shaderProgram.setUniformf("screenSize", width, height);
+        shaderProgram.end();
+    }
+
+    public interface BackgroundRenderable {
+        void setup(BackgroundRenderer renderer);
+        void renderBackground(SpriteBatch batch, Texture background);
+    }
+
+    public static final BackgroundRenderable DEFAULT = new BackgroundRenderable() {
+
+        private BackgroundRenderer renderer;
+
+        @Override
+        public void setup(BackgroundRenderer renderer) {
+            this.renderer = renderer;
+        }
+
+        @Override
+        public void renderBackground(SpriteBatch batch, Texture background) {
+            batch.draw
+        }
+    };
+
+    public ShaderProgram getShaderProgram() {
+        return shaderProgram;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public Vector2 getScreenSize() {
+        return screenSize;
     }
 
     @Override
