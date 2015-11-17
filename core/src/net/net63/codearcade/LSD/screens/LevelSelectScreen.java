d4765df6f3407727a158207a45bb195bd42e6ec7
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-17 20:08:39 +0000

    Began creating level select screen

--- core/src/net/net63/codearcade/LSD/screens/LevelSelectScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/LevelSelectScreen.java
@@ -2,6 +2,8 @@ package net.net63.codearcade.LSD.screens;
 
 import net.net63.codearcade.LSD.LSD;
 import net.net63.codearcade.LSD.utils.BackgroundRenderer;
+import net.net63.codearcade.LSD.utils.CentreGUI;
+import net.net63.codearcade.LSD.utils.LevelManager;
 import net.net63.codearcade.LSD.utils.ShaderManager;
 
 /**
@@ -9,16 +11,33 @@ import net.net63.codearcade.LSD.utils.ShaderManager;
  */
 public class LevelSelectScreen extends AbstractScreen {
 
-    private LSD game;
+    private static final int NUM_COLS = 5;
 
+    private LSD game;
+    private LevelManager.LevelPack levelPack;
+
+    private CentreGUI centreGUI;
     private BackgroundRenderer backgroundRenderer;
 
-    public LevelSelectScreen(LSD game) {
+    public LevelSelectScreen(LSD game, int mapPack) {
         super(game);
 
         this.game = game;
 
+        levelPack = LevelManager.getPack(mapPack);
+
+        centreGUI = new CentreGUI();
         backgroundRenderer = new BackgroundRenderer(ShaderManager.Shaders.MENU, BackgroundRenderer.DEFAULT);
+
+        setupUI();
+    }
+
+    private void setupUI() {
+
+        int rowNum = (int) Math.ceil(levelPack.numLevels / ((double) NUM_COLS));
+        int colNum = NUM_COLS;
+
+        
     }
 
     @Override
@@ -26,6 +45,7 @@ public class LevelSelectScreen extends AbstractScreen {
         super.resize(width, height);
 
         backgroundRenderer.resize(width, height);
+        centreGUI.resize(width, height);
     }
 
     @Override
@@ -33,6 +53,7 @@ public class LevelSelectScreen extends AbstractScreen {
         super.render(deltaTime);
 
         backgroundRenderer.render(deltaTime);
+        centreGUI.render(deltaTime);
     }
 
     @Override
@@ -40,6 +61,7 @@ public class LevelSelectScreen extends AbstractScreen {
         super.dispose();
 
         backgroundRenderer.dispose();
+        centreGUI.dispose();
     }
 
 
