bd6714bdd9dab739d833759592f5ff649b41787f
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-19 18:13:58 +0000

    Finished the button looks!!

--- core/src/net/net63/codearcade/LSD/screens/LevelSelectScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/LevelSelectScreen.java
@@ -1,8 +1,8 @@
 package net.net63.codearcade.LSD.screens;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
 import net.net63.codearcade.LSD.LSD;
 import net.net63.codearcade.LSD.utils.*;
 
@@ -11,14 +11,16 @@ import net.net63.codearcade.LSD.utils.*;
  */
 public class LevelSelectScreen extends AbstractScreen {
 
-    private static final int NUM_COLS = 5;
+    private static final int NUM_COLS = 4;
     private static final int MAX_ROWS = 4;
 
     private static final int PADDING_TOP = 20;
     private static final int PADDING_SIDE = 20;
 
-    private static final int BUTTON_WIDTH = 50;
-    private static final int BUTTON_HEIGHT = 30;
+    private static final int BUTTON_WIDTH = 150;
+    private static final int BUTTON_HEIGHT = 90;
+
+    private static final Color TEXT_COLOR = new Color(100/255f, 100/255f, 100/255f, 1f);
 
     private LSD game;
     private LevelManager.LevelPack levelPack;
@@ -50,13 +52,40 @@ public class LevelSelectScreen extends AbstractScreen {
         Table buttonTable = new Table();
         buttonTable.setPosition(PADDING_SIDE, PADDING_TOP);
         buttonTable.setSize(800 - 2*PADDING_SIDE, title.getY() - 2*PADDING_TOP);
-        buttonTable.setDebug(true);
+        //buttonTable.setDebug(true);
+
+        int cols = 0;
 
         for (int i = 0; i < levelPack.numLevels; i++) {
-            
+
+            ImageButton button = createButton(i);
+
+            buttonTable.add(button)
+                    .width(BUTTON_WIDTH)
+                    .height(BUTTON_HEIGHT)
+                    .uniform()
+                    .fill().expand();
+
+            cols++;
+            if (cols >= NUM_COLS) {
+                buttonTable.row();
+                cols = 0;
+            }
         }
 
         centreGUI.getStage().addActor(buttonTable);
+    }
+
+    private ImageButton createButton(int num) {
+        ImageButton main = GUIBuilder.createButton(Assets.Buttons.LEVEL_SELECT);
+
+        Label text = GUIBuilder.createLabel(Integer.toString(num + 1), Assets.FontSizes.FORTY, TEXT_COLOR);
+        text.setAlignment(Align.center);
+
+        main.clearChildren();
+        main.stack(main.getImage(), text).expand().fill();
+
+        return main;
     }
 
     @Override
