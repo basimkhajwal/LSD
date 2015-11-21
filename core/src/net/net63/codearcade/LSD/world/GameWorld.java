fd4f58f02899be34bdbcfa3889e22e82efb2fcd6
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-21 20:28:14 +0000

    Remove debug render for now

--- core/src/net/net63/codearcade/LSD/world/GameWorld.java
+++ core/src/net/net63/codearcade/LSD/world/GameWorld.java
@@ -203,7 +203,7 @@ public class GameWorld implements Disposable, EntityListener {
         engine.addSystem(new BackgroundRenderSystem());
         engine.addSystem(new ParallaxEffectSystem(gameCamera, levelDescriptor));
         engine.addSystem(new RenderSystem(gameCamera));
-        engine.addSystem(new DebugRenderSystem(gameCamera));
+        //engine.addSystem(new DebugRenderSystem(gameCamera));
         engine.addSystem(new ParticleRenderSystem(gameCamera));
         engine.addSystem(new AimRenderSystem(gameCamera));
     }
