bb7620470ed81d417054ab805e571c630a086865
Basim Khajwal <basimkhajwal@gmail.com>
2015-11-29 14:44:55 +0000

    Created methods

--- core/src/net/net63/codearcade/LSD/events/EventQueue.java
+++ core/src/net/net63/codearcade/LSD/events/EventQueue.java
@@ -3,19 +3,26 @@ package net.net63.codearcade.LSD.events;
 import com.badlogic.ashley.signals.Listener;
 import com.badlogic.ashley.signals.Signal;
 
+import java.util.PriorityQueue;
+
 /**
  * Created by Basim on 28/11/2015.
  */
 public class EventQueue implements Listener<GameEvent> {
 
+    private PriorityQueue<GameEvent> eventQueue;
 
     public EventQueue() {
+        eventQueue = new PriorityQueue<GameEvent>();
+    }
 
+    public GameEvent poll() {
+        return eventQueue.poll();
     }
 
     @Override
     public void receive(Signal<GameEvent> signal, GameEvent event) {
-        
+        eventQueue.add(event);
     }
 
 }
