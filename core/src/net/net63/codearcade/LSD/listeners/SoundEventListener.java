f55601c1ee6025ec2511b3d09bf1a23a210e8879
Basim Khajwal <basimkhajwal@gmail.com>
2015-12-02 20:28:03 +0000

    Created a new file to handle sound events

--- core/src/net/net63/codearcade/LSD/listeners/SoundEventListener.java
+++ core/src/net/net63/codearcade/LSD/listeners/SoundEventListener.java
@@ -3,6 +3,7 @@ package net.net63.codearcade.LSD.listeners;
 import com.badlogic.ashley.signals.Listener;
 import com.badlogic.ashley.signals.Signal;
 import net.net63.codearcade.LSD.events.GameEvent;
+import net.net63.codearcade.LSD.managers.SoundManager;
 
 /**
  * Created by Basim on 02/12/2015.
@@ -11,9 +12,23 @@ public class SoundEventListener implements Listener<GameEvent> {
 
     @Override
     public void receive(Signal<GameEvent> signal, GameEvent event) {
+        String sound = null;
+
         switch (event) {
-            
+
+            case PLAYER_DEATH:
+                sound = SoundManager.Sounds.PLAYER_DEATH;
+                break;
+
+            case LAUNCH_PLAYER:
+                sound = SoundManager.Sounds.EXPLOSION;
+                break;
+
+            default:
+                break;
         }
+
+        if (sound != null) SoundManager.playSound(sound);
     }
 
 }
