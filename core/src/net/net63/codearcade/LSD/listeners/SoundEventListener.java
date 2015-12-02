3740dc7bbfaf4ca09cdf21ef07daabb8325123c6
Basim Khajwal <basimkhajwal@gmail.com>
2015-12-02 20:29:24 +0000

    Added comments

--- core/src/net/net63/codearcade/LSD/listeners/SoundEventListener.java
+++ core/src/net/net63/codearcade/LSD/listeners/SoundEventListener.java
@@ -6,14 +6,19 @@ import net.net63.codearcade.LSD.events.GameEvent;
 import net.net63.codearcade.LSD.managers.SoundManager;
 
 /**
+ * Listener class that listens for game events and plays
+ * the appropriate sound if an event occurs
+ *
  * Created by Basim on 02/12/2015.
  */
 public class SoundEventListener implements Listener<GameEvent> {
 
     @Override
     public void receive(Signal<GameEvent> signal, GameEvent event) {
+        //By default no sound is played
         String sound = null;
 
+        //Depending on the event play a specific sound
         switch (event) {
 
             case PLAYER_DEATH:
@@ -28,6 +33,7 @@ public class SoundEventListener implements Listener<GameEvent> {
                 break;
         }
 
+        //Play the sound if one is being played
         if (sound != null) SoundManager.playSound(sound);
     }
 
