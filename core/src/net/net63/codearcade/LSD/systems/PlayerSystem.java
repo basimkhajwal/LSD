e23576499d33deed70fec65774d22328e1852be2
Basim Khajwal <basimkhajwal@gmail.com>
2015-12-05 17:01:48 +0000

    Commented /re-structured code

--- core/src/net/net63/codearcade/LSD/systems/PlayerSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/PlayerSystem.java
@@ -18,10 +18,14 @@ import net.net63.codearcade.LSD.world.LevelDescriptor;
 import net.net63.codearcade.LSD.world.WorldBuilder;
 
 /**
+ * Player system which handles the abstract and specific
+ * details with controlling the player
+ *
  * Created by Basim on 10/08/15.
  */
 public class PlayerSystem extends IteratingSystem implements ContactListener {
 
+    //Various component mappers
     private ComponentMapper<BodyComponent> bodyMapper;
     private ComponentMapper<StateComponent> stateMapper;
     private ComponentMapper<SensorComponent> sensorMapper;
@@ -29,19 +33,29 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
     private ComponentMapper<WallComponent> wallMapper;
     private ComponentMapper<RenderComponent> renderMapper;
 
+    //Game state
     private LevelDescriptor levelDescriptor;
     private Signal<GameEvent> gameEventSignal;
     private EventQueue eventQueue;
 
+    /**
+     * Create a new player system
+     *
+     * @param levelDescriptor The level descriptor of the world
+     * @param gameEventSignal The event generator of the world
+     */
     public PlayerSystem (LevelDescriptor levelDescriptor, Signal<GameEvent> gameEventSignal) {
         super(Family.all(PlayerComponent.class).get(), Constants.SYSTEM_PRIORITIES.PLAYER);
 
+        //Save instances
         this.levelDescriptor = levelDescriptor;
         this.gameEventSignal = gameEventSignal;
 
+        //Register a new event queue
         eventQueue = new EventQueue();
         gameEventSignal.add(eventQueue);
 
+        //Instantiate mappers
         bodyMapper = ComponentMapper.getFor(BodyComponent.class);
         stateMapper = ComponentMapper.getFor(StateComponent.class);
         playerMapper = ComponentMapper.getFor(PlayerComponent.class);
@@ -52,21 +66,25 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
 
     @Override
     public void processEntity(Entity entity, float deltaTime) {
+        //Get all needed components
         Body body = bodyMapper.get(entity).body;
         StateComponent state = stateMapper.get(entity);
         PlayerComponent playerComponent = playerMapper.get(entity);
         Vector2 position = body.getPosition();
 
+        //Whether or not to kill the player in this tick
         boolean applyDeath = false;
 
+        //Handle incoming events
         for (GameEvent event : eventQueue.getEvents()) {
             switch(event) {
+
+                //Fire the player
                 case LAUNCH_PLAYER:
-                    if (state.get() == PlayerComponent.STATE_AIMING) {
-                        launchPlayer(entity);
-                    }
+                    if (state.get() == PlayerComponent.STATE_AIMING) launchPlayer(entity);
                     break;
 
+                //Colliding with the next platform
                 case PLATFORM_COLLISION:
                     body.setGravityScale(0f);
                     body.setLinearVelocity(0, 0);
@@ -76,24 +94,29 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
                     state.set(PlayerComponent.STATE_STILL);
                     break;
 
+                //Various events that signal player death
                 case WALL_COLLISION:
                 case TIMER_OVER:
-                    if (!playerComponent.isDead) applyDeath = true;
+                    applyDeath = true;
                     break;
 
+                //Explicit death signal (fired by this system)
                 case PLAYER_DEATH:
                     killPlayer(entity);
                     break;
 
+                //Don't handle other events
                 default:
                     break;
             }
         }
 
+        //Update the state to falling (for a different animation) as the player starts moving down
         if (state.get() == PlayerComponent.STATE_JUMPING && body.getLinearVelocity().y < 0) {
             state.set(PlayerComponent.STATE_FALLING);
         }
 
+        //Check if player remains within bounds, appy death otherwise
         if (playerComponent.isFlying && !playerComponent.isDead) {
             Rectangle bounds = levelDescriptor.getWorldBounds();
 
@@ -102,42 +125,60 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
             }
         }
 
-        if (applyDeath) gameEventSignal.dispatch(GameEvent.PLAYER_DEATH);
+        //Apply death (if the player isn't already dead)
+        if (applyDeath && !playerComponent.isDead) gameEventSignal.dispatch(GameEvent.PLAYER_DEATH);
     }
 
+    /**
+     * Launch the player to the touch coordinates
+     */
     private void launchPlayer(Entity player) {
+        //Get components
         PlayerComponent playerComponent = playerMapper.get(player);
         Body body = bodyMapper.get(player).body;
 
+        //Re-apply gravity apply appropriate launch impulse
         body.setGravityScale(1.0f);
         body.applyLinearImpulse(playerComponent.launchImpulse, body.getWorldCenter(), true);
 
+        //Destroy the sensor on which the player is on
         if (playerComponent.currentSensor != null) {
             getEngine().removeEntity(playerComponent.currentSensor);
-
-            playerComponent.isFlying = true;
-            playerComponent.currentSensor = null;
         }
 
+        //Apply the new state
+        playerComponent.isFlying = true;
+        playerComponent.currentSensor = null;
         stateMapper.get(player).set(PlayerComponent.STATE_JUMPING);
     }
 
+    /**
+     * Kill the player
+     *
+     * @param player The entity
+     */
     private void killPlayer(Entity player) {
         Body body = bodyMapper.get(player).body;
 
+        //Set the state variables
         playerMapper.get(player).isDead = true;
         renderMapper.get(player).render = false;
         stateMapper.get(player).set(PlayerComponent.STATE_DEAD);
 
         // --- Particle Effect ----
+
+        //How many particles to generate
         int num = Constants.PLAYER_DEATH_PARTICLES;
 
+        //Position and radius of player
         Vector2 pos = body.getPosition();
         float radius = body.getFixtureList().first().getShape().getRadius();
 
+        //Each particle start position and color
         Vector2[] positions = new Vector2[num];
         Color[] colors = new Color[num];
 
+        //Randomly position within player radius
         for (int i = 0; i < num; i++) {
             positions[i] = new Vector2(pos);
             positions[i].add(MathUtils.random(-radius, radius), MathUtils.random(-radius, radius));
@@ -145,6 +186,7 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
             colors[i] = Color.BROWN;
         }
 
+        //Create particle effect from builder
         WorldBuilder.createParticleEffect(positions, colors, 2, 5, 5);
     }
 
@@ -153,28 +195,37 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
 
+        //Get bodies
         Body a = contact.getFixtureA().getBody();
         Body b = contact.getFixtureB().getBody();
 
+        //Check if entities have collided
         Entity entityA = (Entity) a.getUserData();
         Entity entityB = (Entity) b.getUserData();
         Entity playerEntity = null;
 
+        //End if no entity-entity collision occured
         if (entityA == null || entityB == null) return;
 
+        //Find player entity
         if (playerMapper.has(entityA)) playerEntity = entityA;
         if (playerMapper.has(entityB)) playerEntity = entityB;
 
-        Entity other = (entityA == playerEntity) ? entityB : entityA;
-
+        //If a player collision occurred
         if (playerEntity != null) {
+            //Get the player component
             PlayerComponent playerComponent = playerMapper.get(playerEntity);
 
+            //Other entity (if player found)
+            Entity other = (entityA == playerEntity) ? entityB : entityA;
+
+            //Fire event that sensor was collided with and save the sensor
             if (sensorMapper.has(other)) {
                 gameEventSignal.dispatch(GameEvent.PLATFORM_COLLISION);
                 playerComponent.currentSensor = other;
             }
 
+            //Fire event that a wall was collided with
             if (wallMapper.has(other)) {
                 gameEventSignal.dispatch(GameEvent.WALL_COLLISION);
             }
@@ -182,6 +233,7 @@ public class PlayerSystem extends IteratingSystem implements ContactListener {
         }
     }
 
+    // ------------------ Un-used contact functions ------------------
     @Override
     public void endContact(Contact contact) { }
     @Override
