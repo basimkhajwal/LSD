8e5518ee1c432298e9e84854b9f64de6cd9d17e0
Basim Khajwal <basimkhajwal@gmail.com>
2015-12-23 12:26:44 +0000

    Added code for the cross button

--- core/src/net/net63/codearcade/LSD/screens/overlays/SettingsScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/overlays/SettingsScreen.java
@@ -1,27 +1,28 @@
 package net.net63.codearcade.LSD.screens.overlays;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import net.net63.codearcade.LSD.LSD;
 import net.net63.codearcade.LSD.managers.Assets;
 import net.net63.codearcade.LSD.screens.AbstractScreen;
+import net.net63.codearcade.LSD.utils.GUIBuilder;
 
 /**
  * Created by Basim on 21/12/15.
  */
 public class SettingsScreen extends AbstractOverlay {
 
-
+    private ImageButton crossButton;
 
     public SettingsScreen(LSD game, AbstractScreen previousScreen) {
         super(game, previousScreen);
 
         disposeScreen = false;
-
-
-
     }
 
     @Override
@@ -29,14 +30,27 @@ public class SettingsScreen extends AbstractOverlay {
 
         NinePatch bg = new NinePatch(Assets.getAsset(Assets.Images.SETTINGS_BACKGROUND, Texture.class), 9, 9, 9, 9);
         Image background = new Image(bg);
-        background.setSize(700, 500);
-        background.setPosition(50, 50);
+        background.setSize(400, 500);
+        background.setPosition(200, 50);
+
+        Label title = GUIBuilder.createLabel("Settings", Assets.FontSizes.FORTY, Color.MAROON);
+        title.setPosition((800 - title.getWidth()) / 2, 550 - title.getHeight() - 20);
+
+        crossButton = GUIBuilder.createButton(Assets.Buttons.CROSS);
+        crossButton.setSize(30, 30);
+        crossButton.setPosition(background.getX() + background.getWidth() - crossButton.getWidth() / 2,
+                background.getY() + background.getHeight() - crossButton.getHeight() / 2);
+
         stage.addActor(background);
-        
+        stage.addActor(title);
+        stage.addActor(crossButton);
+
     }
 
     @Override
     public void checkChange() {
+
+        crossButton.setChecked(crossButton.isOver());
 
     }
 
