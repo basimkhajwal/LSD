736a8993c6997a1a980db884e21a8690fe2a1dc5
Basim Khajwal <basimkhajwal@gmail.com>
2015-12-28 14:04:18 +0000

    Added basic definition

--- core/src/net/net63/codearcade/LSD/components/NodeMovementComponent.java
+++ core/src/net/net63/codearcade/LSD/components/NodeMovementComponent.java
@@ -1,12 +1,20 @@
 package net.net63.codearcade.LSD.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Basim on 28/12/15.
  */
 public class NodeMovementComponent implements Component {
 
+    public float delay;
+    public float timeRemaining;
+    public boolean hasStarted = false;
 
+    public boolean movingForward = true;
+    public float speed;
 
+    public Vector2[] nodes;
+    public int currentNode;
 }
