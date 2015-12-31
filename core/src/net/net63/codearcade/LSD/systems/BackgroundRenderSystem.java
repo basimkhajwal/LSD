940ae29a99d57a7c6eb24aa2623051d79822adf8
Basim Khajwal <basimkhajwal@gmail.com>
2015-12-31 12:57:18 +0000

    Used the saturation parameter to increase as time decreases

--- core/src/net/net63/codearcade/LSD/systems/BackgroundRenderSystem.java
+++ core/src/net/net63/codearcade/LSD/systems/BackgroundRenderSystem.java
@@ -1,14 +1,16 @@
 package net.net63.codearcade.LSD.systems;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.signals.Signal;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.utils.Disposable;
 import net.net63.codearcade.LSD.events.EventQueue;
 import net.net63.codearcade.LSD.events.GameEvent;
+import net.net63.codearcade.LSD.managers.ShaderManager;
 import net.net63.codearcade.LSD.utils.BackgroundRenderer;
 import net.net63.codearcade.LSD.utils.Constants;
-import net.net63.codearcade.LSD.managers.ShaderManager;
 
 /**
  * Render the background for the game, using a separate
@@ -18,9 +20,15 @@ import net.net63.codearcade.LSD.managers.ShaderManager;
  */
 public class BackgroundRenderSystem extends EntitySystem implements Disposable {
 
-    private BackgroundRenderer backgroundRenderer;
+    private Engine engine;
+    private TimerSystem timerSystem;
 
+    private BackgroundRenderer backgroundRenderer;
     private EventQueue eventQueue;
+
+    private float saturation = 0.1f;
+    private boolean paused = false;
+    private boolean timerOn = false;
 
     public BackgroundRenderSystem(Signal<GameEvent> signal) {
         super(Constants.SYSTEM_PRIORITIES.BACKGROUND_RENDER);
@@ -29,17 +37,70 @@ public class BackgroundRenderSystem extends EntitySystem implements Disposable {
         signal.add(eventQueue);
 
         backgroundRenderer = new BackgroundRenderer(ShaderManager.Shaders.GAME, BackgroundRenderer.DEFAULT);
+        updateSaturation();
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        this.engine = engine;
     }
 
     @Override
     public void update(float deltaTime) {
+        if (timerSystem == null) timerSystem = engine.getSystem(TimerSystem.class);
+
         for (GameEvent event : eventQueue.getEvents()) {
-            if (event == GameEvent.RESIZE) {
-                backgroundRenderer.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            switch (event) {
+
+                case RESIZE:
+                    backgroundRenderer.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+                    break;
+
+                case PAUSE_GAME:
+                    paused = true;
+                    resetSaturation();
+                    break;
+
+                case RESUME_GAME:
+                    paused = false;
+                    break;
+
+                case TIMER_STARTED:
+                    timerOn = true;
+                    resetSaturation();
+                    break;
+
+                case TIMER_STOPPED:
+                    timerOn = false;
+                    resetSaturation();
+                    break;
+
             }
+
+
+        }
+
+        if (!paused && timerOn) {
+            saturation = timerSystem.getCurrentTime() / timerSystem.getCurrentMaxTime();
+            saturation = Math.max(0.1f, saturation);
+
+            updateSaturation();
         }
 
         backgroundRenderer.render(deltaTime);
+    }
+
+    private void resetSaturation() {
+        saturation = 0.1f;
+        updateSaturation();
+    }
+
+    private void updateSaturation() {
+        ShaderProgram shader = backgroundRenderer.getShaderProgram();
+
+        shader.begin();
+        shader.setUniformf("saturation", saturation);
+        shader.end();
     }
 
     @Override
