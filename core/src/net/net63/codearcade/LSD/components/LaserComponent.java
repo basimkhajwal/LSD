84e2aa4c76da5359e76e3ab6715925ca40857cd1
Basim Khajwal <basimkhajwal@gmail.com>
2016-01-02 14:45:35 +0000

    Added comments and a default value for laser enabled

--- core/src/net/net63/codearcade/LSD/components/LaserComponent.java
+++ core/src/net/net63/codearcade/LSD/components/LaserComponent.java
@@ -10,8 +10,10 @@ public class LaserComponent implements Component {
     //Between 0 and 360 clockwise with 0 pointing right
     public float angle;
     public float interval;
+
+    //0 - up, 1 - right, 2 - down, 3 - left
     public int direction;
 
     public float currentTime = 0;
-    public boolean laserEnabled;
+    public boolean laserEnabled = false;
 }
