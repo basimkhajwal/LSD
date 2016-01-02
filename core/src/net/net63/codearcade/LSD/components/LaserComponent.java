3c49f55811307abf33c9f9802658fc9871930fbc
Basim Khajwal <basimkhajwal@gmail.com>
2016-01-02 21:22:27 +0000

    Added new variables for the laser draw position

--- core/src/net/net63/codearcade/LSD/components/LaserComponent.java
+++ core/src/net/net63/codearcade/LSD/components/LaserComponent.java
@@ -1,6 +1,8 @@
 package net.net63.codearcade.LSD.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+import net.net63.codearcade.LSD.utils.Constants;
 
 /**
  * Created by Basim on 31/12/15.
@@ -14,6 +16,9 @@ public class LaserComponent implements Component {
     //0 - up, 1 - right, 2 - down, 3 - left
     public int direction;
 
-    public float currentTime = 0;
+    public float laserTime = 0;
     public boolean laserEnabled = false;
+
+    public float laserUpdateTime = Constants.LASER_UPDATE_TIME;
+    public Vector2 laserEndPos = new Vector2();
 }
