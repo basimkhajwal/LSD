c8c7b80d7e2316bbd5c76b0577fe3daf85aae02f
Basim Khajwal <basimkhajwal@gmail.com>
2016-01-20 20:29:00 +0000

    Created star component

---
+++ core/src/net/net63/codearcade/LSD/components/StarComponent.java
@@ -0,0 +1,12 @@
+package net.net63.codearcade.LSD.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by Basim on 20/01/16.
+ */
+public class StarComponent implements Component {
+
+    public int index; //0, 1, 2 depending on which star it is
+
+}
