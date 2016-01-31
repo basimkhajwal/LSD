b00dd7cadfe86121229d2c5a5391f0760df63cca
Basim Khajwal <basimkhajwal@gmail.com>
2016-01-31 19:40:41 +0000

    Created new yes no dialog screen

---
+++ core/src/net/net63/codearcade/LSD/screens/overlays/YesNoDialogScreen.java
@@ -0,0 +1,26 @@
+package net.net63.codearcade.LSD.screens.overlays;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import net.net63.codearcade.LSD.LSD;
+import net.net63.codearcade.LSD.screens.AbstractScreen;
+
+/**
+ * Created by Basim on 31/01/16.
+ */
+public class YesNoDialogScreen extends AbstractOverlay {
+
+    public YesNoDialogScreen(LSD game, AbstractScreen previousScreen) {
+        super(game, previousScreen);
+
+    }
+
+    @Override
+    public void setupUI(Stage stage) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
