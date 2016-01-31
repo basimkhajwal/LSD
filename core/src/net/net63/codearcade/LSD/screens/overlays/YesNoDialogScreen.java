21a022700ac2572fffab45c4e71a8db23b0b5da4
Basim Khajwal <basimkhajwal@gmail.com>
2016-01-31 19:42:34 +0000

    added some basic stuff nothing much

--- core/src/net/net63/codearcade/LSD/screens/overlays/YesNoDialogScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/overlays/YesNoDialogScreen.java
@@ -1,7 +1,11 @@
 package net.net63.codearcade.LSD.screens.overlays;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import net.net63.codearcade.LSD.LSD;
+import net.net63.codearcade.LSD.managers.Assets;
 import net.net63.codearcade.LSD.screens.AbstractScreen;
 
 /**
@@ -9,13 +13,27 @@ import net.net63.codearcade.LSD.screens.AbstractScreen;
  */
 public class YesNoDialogScreen extends AbstractOverlay {
 
-    public YesNoDialogScreen(LSD game, AbstractScreen previousScreen) {
+    private String title;
+    private String message;
+
+    public YesNoDialogScreen(LSD game, AbstractScreen previousScreen, String title, String message) {
         super(game, previousScreen);
 
+        this.title = title;
+        this.message = message;
     }
 
     @Override
     public void setupUI(Stage stage) {
+
+        NinePatch bg = new NinePatch(Assets.getAsset(Assets.Images.SETTINGS_BACKGROUND, Texture.class), 9, 9, 9, 9);
+        final Image background = new Image(bg);
+        background.setSize(400, 500);
+        background.setPosition(200, 50);
+
+
+
+        stage.addActor(background);
 
     }
 
