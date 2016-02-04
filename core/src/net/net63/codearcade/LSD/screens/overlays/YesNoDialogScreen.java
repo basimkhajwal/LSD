7e8415e142653b8accbabfabcf7aa82f12936d30
Basim Khajwal <basimkhajwal@gmail.com>
2016-02-04 21:50:20 +0000

    Added text and a wrapper for it

--- core/src/net/net63/codearcade/LSD/screens/overlays/YesNoDialogScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/overlays/YesNoDialogScreen.java
@@ -1,12 +1,18 @@
 package net.net63.codearcade.LSD.screens.overlays;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import net.net63.codearcade.LSD.LSD;
 import net.net63.codearcade.LSD.managers.Assets;
 import net.net63.codearcade.LSD.screens.AbstractScreen;
+import net.net63.codearcade.LSD.utils.GUIBuilder;
 
 /**
  * Created by Basim on 31/01/16.
@@ -26,19 +32,32 @@ public class YesNoDialogScreen extends AbstractOverlay {
     @Override
     public void setupUI(Stage stage) {
 
+        Table contentTable = new Table();
+        Table wrapperTable = new Table();
+
+        Label titleLabel = GUIBuilder.createLabel(title, Assets.FontSizes.THIRTY, Color.DARK_GRAY);
+        Label textLabel = GUIBuilder.createLabel(message, Assets.FontSizes.TWENTY, Color.GRAY);
+        textLabel.setWrap(true);
+        textLabel.setAlignment(Align.center);
+
+        contentTable.add(titleLabel).padLeft(50).padRight(50).padTop(20).center();
+        contentTable.row();
+        contentTable.add(textLabel).fillX().center().pad(20);
+
         NinePatch bg = new NinePatch(Assets.getAsset(Assets.Images.SETTINGS_BACKGROUND, Texture.class), 9, 9, 9, 9);
         final Image background = new Image(bg);
-        background.setSize(400, 500);
-        background.setPosition(200, 50);
+        background.setSize(contentTable.getWidth(), contentTable.getHeight());
+        wrapperTable.stack(background, contentTable);
+        wrapperTable.setPosition( (800 - background.getWidth()) / 2, (600 - background.getHeight()) / 2);
 
-
-
-        stage.addActor(background);
-
+        stage.addActor(wrapperTable);
     }
 
     @Override
     public void update() {
-
+        if (Gdx.input.justTouched()) {
+            previousScreen.resumeLogic();
+            game.setScreen(previousScreen);
+        }
     }
 }
