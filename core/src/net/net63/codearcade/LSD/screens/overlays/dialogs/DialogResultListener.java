2451e1bc8204b82e8443210cf2041a107dbf90ba
Basim Khajwal <basimkhajwal@gmail.com>
2016-02-08 20:55:14 +0000

    Added respective listener interface

---
+++ core/src/net/net63/codearcade/LSD/screens/overlays/dialogs/DialogResultListener.java
@@ -0,0 +1,13 @@
+package net.net63.codearcade.LSD.screens.overlays.dialogs;
+
+/**
+ * Listener interface that classes must implement to be valid listeners
+ * for a dialog event
+ *
+ * Created by Basim on 08/02/16.
+ */
+public interface DialogResultListener {
+
+    void handleResult(DialogResult data);
+
+}
