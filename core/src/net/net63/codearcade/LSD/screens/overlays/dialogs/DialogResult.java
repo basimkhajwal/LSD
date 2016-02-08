30ca5ec73f79520bf25234f0a7ddc496e098456a
Basim Khajwal <basimkhajwal@gmail.com>
2016-02-08 20:55:05 +0000

    Created enum to represent return results

---
+++ core/src/net/net63/codearcade/LSD/screens/overlays/dialogs/DialogResult.java
@@ -0,0 +1,16 @@
+package net.net63.codearcade.LSD.screens.overlays.dialogs;
+
+/**
+ * Enum to represent the various results a dialog can return to
+ * the class that called it
+ *
+ * Created by Basim on 08/02/16.
+ */
+public enum DialogResult {
+
+    CLOSED,
+
+    YES,
+    NO;
+
+}
