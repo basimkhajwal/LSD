63c7195bec5bcb52888bb14acd984b72a1ea39c4
Basim Khajwal <basimkhajwal@gmail.com>
2016-05-29 19:43:00 +0100

    Began implementing the LoadingScreen

--- core/src/net/net63/codearcade/LSD/screens/LoadingScreen.java
+++ core/src/net/net63/codearcade/LSD/screens/LoadingScreen.java
@@ -1,30 +1,42 @@
 package net.net63.codearcade.LSD.screens;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import net.net63.codearcade.LSD.LSD;
-import net.net63.codearcade.LSD.managers.ShaderManager;
-import net.net63.codearcade.LSD.utils.BackgroundRenderer;
+import net.net63.codearcade.LSD.managers.Assets;
 import net.net63.codearcade.LSD.utils.CentreGUI;
+import net.net63.codearcade.LSD.utils.GUIBuilder;
 
 /**
  * Created by Basim on 29/05/16.
  */
 public class LoadingScreen extends AbstractScreen {
 
-    private BackgroundRenderer backgroundRenderer;
     private CentreGUI centreGUI;
 
     public LoadingScreen(LSD game) {
         super(game);
+        this.clear = Color.LIGHT_GRAY;
 
-        backgroundRenderer = new BackgroundRenderer(ShaderManager.Shaders.MENU, BackgroundRenderer.DEFAULT);
         centreGUI = new CentreGUI();
+        setupUI(centreGUI.getStage());
+    }
+
+    private void setupUI(Stage stage) {
+
+        Label title = GUIBuilder.createLabel("Loading", Assets.FontSizes.HUNDRED, Color.DARK_GRAY);
+        title.setPosition(400, 400, Align.center);
+
+        stage.addActor(title);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
 
-
+        centreGUI.render(delta);
     }
 
     @Override
@@ -32,7 +44,6 @@ public class LoadingScreen extends AbstractScreen {
         super.dispose();
 
         centreGUI.dispose();
-        backgroundRenderer.dispose();
     }
 
 }
